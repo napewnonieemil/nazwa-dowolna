349a4c188ce348ef3f354b38b7f4fa6a5dcaca32
DanielVeB <danielkurosz@gmail.com>
2017-11-28 19:38:00 +0100

    5 \\// //\\

---
+++ 5.c
@@ -0,0 +1,29 @@
+// Liczby podzielne --- zmiana
+// Data   : 11.03.2008 --- zmiana 2
+// (C)2012 mgr Jerzy Wa³aszek
+//----------------------------
+
+#include <iostream>
+
+using namespace std;
+
+const int MAXP = 1000;
+
+int main()
+{
+  int a,b,i,j,n,P[MAXP];
+
+  cin >> a >> b >> n;
+  for(i = 0; i < n; i++)
+    cin >> P[i];
+
+  for(i = a; i <= b; i++)
+    for(j = 0; j < n; j++)
+      if(i % P[j] == 0)
+      {
+        cout << i << " ";
+        break;
+      }
+  cout << endl;
+  return 0;
+}
