d513a1ce75334999f5ec83b5f934dba06d0b3ca7
DanielVeB <danielkurosz@gmail.com>
2017-11-28 19:37:32 +0100

    4 /\/\/\/\\/\/\/\

---
+++ 4.c
@@ -0,0 +1,42 @@
+// Rozk³ad na czynniki pierwsze --- zmiana
+// Data   : 24.03.2008 --- zmiana 2
+// (C)2012 mgr Jerzy Wa³aszek
+//----------------------------
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  unsigned int p,i,g,k;
+  int d;
+
+  cin >> p;
+  g = (unsigned int)sqrt(p);
+  i = 2; k = 1; d = -1;
+
+  while(i <= g)
+  {
+    while(!(p % i))
+    {
+      cout << i << " ";
+      p /= i;
+    }
+    if(p == 1) break;
+    if(i < 3) i++;
+    else
+    {
+      i = 6 * k + d;
+      if(d == 1)
+      {
+        d = -1; k++;
+      }
+      else d = 1;
+    }
+  }
+  if(p > 1) cout << p;
+  cout << endl;
+  return 0;
+}
