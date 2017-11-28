8460b8799e84aeaea21d08f3eb999a87ba9d91e4
DanielVeB <danielkurosz@gmail.com>
2017-11-28 19:37:05 +0100

    3 ===-=--0fjcn shaks

---
+++ 3.c
@@ -0,0 +1,23 @@
+// Liczby Fibonacciego --- zmiana
+// Data:   20.04.2008 --- zmiana 2
+// (C)2012 mgr Jerzy Wa³aszek
+//---------------------------
+
+#include <iostream>
+
+using namespace std;
+
+unsigned long fibo(int n)
+{
+  if(n <= 1) return n;
+  else return fibo(n - 2) + fibo(n - 1);
+}
+
+int main()
+{
+  int n;
+
+  cin >> n;
+  cout << fibo(n) << "\n\n";
+  return 0;
+}
