0b3bdd567cfb214bf12d2c6d4b0807058ce37c44
napewnonieemil <34072103+napewnonieemil@users.noreply.github.com>
2017-11-28 20:25:25 +0100

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,6 +1,6 @@
-// Liczby podzielne --- zmiana
+// Liczby podzielne --- zmianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 // Data   : 11.03.2008 --- zmiana 2
-// (C)2012 mgr Jerzy Wa³aszek
+// (C)2012 mgr Jerzy WaÂ³aszek
 //----------------------------
 
 #include <iostream>
